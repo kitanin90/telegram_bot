e34cfcc52dc1d3b87ec3dd826aaaa6cff91e9289
kitanin90 <kitanin90@mail.ru>
2019-10-20 01:03:04 +0500

    add SOCKS5 proxy

--- bot/echo_bot.py
+++ bot/echo_bot.py
@@ -1,3 +1,30 @@
-import telebot
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
-bot = telebot.TeleBot("959039109:AAE0sPZPtj_bfbgs3zUhYhP78UKivIXo19w")
+import telebot, time
+
+from telebot import apihelper
+
+#Configure
+TOKEN = '959039109:AAE0sPZPtj_bfbgs3zUhYhP78UKivIXo19w'
+PROXY = 'socks5://162.243.32.120:33727'
+
+apihelper.proxy = {'https': PROXY}
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, 'Привет, как дела?')
+
+
+@bot.message_handler(func=lambda m:True)
+def echo_all(message):
+	bot.reply_to(message.message.text)
+
+
+while True:
+	try:
+		bot.polling(none_stop=True)
+	except Exception:
+		print('Что-то не работает')
